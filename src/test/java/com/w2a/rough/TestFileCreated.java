54d03b4c048ff5dfbb5185aefc54b21813e00022
Sonal Agarwal <sonal.fd@gmail.com>
2021-01-17 17:21:10 +0530

    Commit from owner again

--- src/test/java/com/w2a/rough/TestFileCreated.java
+++ src/test/java/com/w2a/rough/TestFileCreated.java
@@ -1,8 +1,19 @@
 package com.w2a.rough;
 
-import org.bson.io.BsonOutput;
+
+import org.testng.annotations.Test;
 
 public class TestFileCreated {
 
+@Test
+
+    public void abc()
+{
+    System.out.println("surefire error");
+}
+    public void abc1()
+    {
+        System.out.println("surefire error1");
+    }
 
 }
