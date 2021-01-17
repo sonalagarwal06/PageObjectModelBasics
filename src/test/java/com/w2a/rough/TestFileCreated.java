db22d8f14d805ce8e8bfbb1f9bb75a296001ccb5
psheoran783 <psheoran783@gmail.com>
2021-01-17 16:14:47 +0530

    first commit in new branch

--- src/test/java/com/w2a/rough/TestFileCreated.java
+++ src/test/java/com/w2a/rough/TestFileCreated.java
@@ -1,8 +1,13 @@
 package com.w2a.rough;
 
 import org.bson.io.BsonOutput;
+import org.testng.annotations.Test;
 
 public class TestFileCreated {
 
-
+  @Test
+    public void test3WayMerge()
+  {
+      System.out.println("Jui is mad, cow buffalo cat");
+  }
 }
